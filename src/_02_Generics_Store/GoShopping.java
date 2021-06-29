195b8d47d7750f1dfed68211c1e8bb3c7e981d7d
codybsauer <47398539+codybsauer@users.noreply.github.com>
2021-06-29 14:29:22 -0700

    first commit

---
+++ src/_02_Generics_Store/GoShopping.java
@@ -0,0 +1,25 @@
+package _02_Generics_Store;
+
+public class GoShopping {
+	public static void main(String[] args) {
+		// 1. Create 2 Cart objects(Candy, Cereal)
+
+		// 2. Add items to each cart
+
+		// 3. Call the showCart() method on each cart
+
+		// 4. Run the code
+		
+		// 5. Comment out the code you have so far
+		
+		// 6. Repeat steps 1-3 for a Clothing Cart and a Toy Cart
+		
+		// Why are there errors?
+		
+		// 7. Modify the Cart class so that this code compiles
+		// HINT: There are 3 lines of code to edit
+				
+		// 8. Run the code. Does it work?
+		
+	}
+}
